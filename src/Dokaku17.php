89753a267af9e6ecc0e47c93b7ba0355613c3998
Takashi Kanemoto <ttskch@gmail.com>
2019-08-27 01:14:06 +0900

    impl smartly

--- src/Dokaku17.php
+++ src/Dokaku17.php
@@ -8,7 +8,22 @@ class Dokaku17
 {
     public function run(string $input) : string
     {
-        // implement me
-        return '1444';
+        $dec = intval($input) + 1;
+
+        while (true) {
+            $bin = '0' . decbin($dec);
+
+            preg_match('/(.*?)(1000|0111)(.*)/', $bin, $m);
+
+            if (empty($m)) {
+                return strval($dec);
+            }
+
+            $head = ltrim($m[1], '0');
+            $tailLength = strlen($m[3]);
+            $tail = substr(str_repeat('001', intval(floor($tailLength / 3)) + 1), 0, $tailLength);
+
+            $dec = strval(bindec($head . '1001' . $tail));
+        }
     }
 }
